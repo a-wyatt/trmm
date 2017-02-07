94fe15d420b8b5e8bc891926bdd763457c13fa01
a-wyatt <andrewwyatt90@gmail.com>
2017-02-07 23:06:06 +0000

    update to basic plot.py

--- basic_plot.py
+++ basic_plot.py
@@ -1,12 +1,15 @@
-#basic_plot
-
+#!/usr/bin/env python
 import pandas as pd
-
 import matplotlib.pyplot as plt
-from matplotlib.mlab import griddata
-from mpl_toolkits.basemap import Basemap, addcyclic, shiftgrid
+from mpl_toolkits.basemap import Basemap
+import glob
 
-df = pd.read_csv('averages/clipped/0528.csv')
+files = glob.glob('averages/clipped/09*.csv')
+
+#df = pd.read_csv('averages/clipped/%s.csv' % (s))
+frames = [ pd.read_csv(f, index_col=['lon', 'lat']) for f in files ]
+result = pd.concat(frames, keys=files)
+averages = result.groupby(level=[1, 2]).mean().reset_index()
 
 fig = plt.figure()
 
@@ -16,6 +19,6 @@ m = Basemap(projection='cyl', llcrnrlat=5.0, urcrnrlat=35.0,\
 m.drawcoastlines()
 m.drawmapboundary()
 
-m.scatter(x=df.lon.values, y=df.lat.values, c=df.precipitation.values, s=5.0)
+m.scatter(x=averages.lon.values, y=averages.lat.values, c=averages.precipitation.values, s=5.0)
 
 plt.show()
